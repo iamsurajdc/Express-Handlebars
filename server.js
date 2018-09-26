615cee91d24a0960190f37cf5f2801d563cb8fbf
Suraj Chandgude <surajchandgude0304@gmail.com>
2018-09-26 12:35:03 +0530

    maintainance middleware configured added

--- server.js
+++ server.js
@@ -5,12 +5,12 @@ var app = express();
 
 hbs.registerPartials(__dirname + "/views/partials")
 
+// app.use((req, res, next) => {
+//     res.render('maintainance.hbs');
+// });
+
 app.set('view engine', 'hbs');
 app.use(express.static(__dirname));
-
-app.use((req, res, next) => {
-    res.render('maintainance.hbs');
-});
 
 hbs.registerHelper('getCurrentYear', () => {
     return new Date().getFullYear();
