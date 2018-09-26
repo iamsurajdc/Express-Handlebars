1924c9c7d0643cf338edf8d3ec40d1b8eef6fd2d
Suraj Chandgude <surajchandgude0304@gmail.com>
2018-09-26 15:33:08 +0530

    setup the heroku and dynamic port

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
 const express = require("express");
 const hbs = require('hbs');
+const port = process.env.PORT || 3000;  
 var app = express();
 
 hbs.registerPartials(__dirname + "/views/partials")
